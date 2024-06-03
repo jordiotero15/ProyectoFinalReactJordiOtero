fad0943bede9df0d50004ecf2d694cfd948e1c2b
jordiotero15 <jordiotero15@gmail.com>
2024-06-03 14:54:26 -0300

    Detalles de los productos

--- src/componentes/ItemDetailContainer.jsx
+++ src/componentes/ItemDetailContainer.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Button } from 'antd';
 import { useParams } from 'react-router-dom';
+import { RiArrowDropDownLine, RiArrowDropUpLine } from "react-icons/ri";
+
 
 
 
@@ -25,6 +27,7 @@ function ItemDetailContainer() {
       })
   }, [id]);
 
+  const [isOpen, setIsOpen] = useState(false);
   return (
 
     <div className='flex items-center flex-col '>
@@ -39,12 +42,34 @@ function ItemDetailContainer() {
               <h4 className='p-5 font-semibold text-3xl'>$ {producto.precio}</h4>
               <p className='p-5 text-justify'>{producto.description}</p>
             </div>
-            <div className='border p-5'>
-              <div className='flex flex-col gap-1'>
+            <div className='border p-5 '>
+              <div className='flex flex-col gap-3'>
                 <h5 className='font-semibold'>Stock disponible</h5>
-                <p>Cantidad:</p>
-                <button className='bg-blue-500 text-white hover:bg-blue-600 text-lg font-bold transition-all duration-500 h-10 rounded'>Comprar ahora</button>
-                <Button className='text-blue-500 text-lg font-bold h-10'>Agregar al carrito</Button>
+                <div className='relative'>
+                  <button onClick={() => setIsOpen((prev) => !prev)} className='flex gap-x-1'>Cantidad:<span className='font-semibold'>1 unidad</span>
+                    {!isOpen ? (
+                      <RiArrowDropDownLine className='text-2xl text-blue-500 ' />
+                    ) : (
+                      <RiArrowDropUpLine className='text-2xl text-blue-500 ' />
+                    )}
+                  </button>
+                  {isOpen && <div className='absolute'>
+                    <ul>
+                      <li>1 unidad</li>
+                      <li>2 unidades</li>
+                      <li>3 unidades</li>
+                      <li>4 unidades</li>
+                      <li>5 unidades</li>
+                      <li>6 unidades</li>
+                    </ul>
+                  </div>
+                  }
+                </div>
+                <div className='flex flex-col gap-1'>
+                  <button className='bg-blue-500 text-white hover:bg-blue-600 text-lg font-bold transition-all duration-500 h-10 rounded'>Comprar ahora</button>
+                  <button className='text-blue-500 text-lg font-bold h-10'>Agregar al carrito</button>
+                  {/* <Button className='text-blue-500 text-lg font-bold h-10'>Agregar al carrito</Button> */}
+                </div>
               </div>
             </div>
           </div>
